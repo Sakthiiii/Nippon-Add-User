c61209c5302fdc1a046bfba172230efe26d53405
Sakthi <87663226+Sakthiiii@users.noreply.github.com>
2022-05-21 19:07:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import Home from './Home'
 import './App.css';
 
 function App() {
-  const Prolang=['Bharathi','Ansari','chinna','Dhanabal','Elango','Francies','Davit','Sakthi','Preeti','Kavitha','Vicky']
+ 
   return (
     <div className="App">
-     <Home lang={Prolang}/>
+     <Home />
     </div>
   );
 }
